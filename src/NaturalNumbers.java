c74ebec4e86421f117eaacc7d33cd8ee8505be77
Freakzz26 <madhumithamurali02@gmail.com>
2023-07-19 23:46:31 +0530

    Changes done

--- src/NaturalNumbers.java
+++ src/NaturalNumbers.java
@@ -3,13 +3,20 @@ import java.util.Scanner;
 public class NaturalNumbers {
     static void natural(int number)
     {
+
         if(number>1) {
             natural(number-1);
             System.out.print(" "+number+" ");
+            if(number>2)
+            {
+                System.out.println('\n');
+            }
         }
+
         else {
             System.out.print(1);
         }
+
     }
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
