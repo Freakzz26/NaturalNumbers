fc90101df9ff30cc98a22d6d8387c42341dc718d
Freakzz26 <madhumithamurali02@gmail.com>
2023-07-19 23:35:11 +0530

    Initial commit done

---
+++ src/NaturalNumbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class NaturalNumbers {
+    static void natural(int number)
+    {
+        if(number>1) {
+            natural(number-1);
+            System.out.print(" "+number+" ");
+        }
+        else {
+            System.out.print(1);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int number=scanner.nextInt();
+        natural(number);
+    }
+}
